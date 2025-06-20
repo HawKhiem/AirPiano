8277a3dcd1071baf2e48c203e3e7c30bca73584d
Khiem Nguyen <ge48hid@tum.de>
2025-06-20 13:49:02 +0200

    implement pressing buttons mechanism, make buttons transparent and adjust size, color

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import cv2
+import cvzone
+import numpy as np
 from cvzone.HandTrackingModule import HandDetector
 
 cap = cv2.VideoCapture(0)
@@ -8,19 +10,30 @@ cap.set(4, 1440)
 # higher detectionCon to avoid randomly key pressed
 detector = HandDetector(detectionCon=0.8)
 
+
 # draw the button onto the image every iteration, since each iteration we have a new image
-def drawAll(img, buttonList):
+def drawAllTransparent(img, buttonList):
+    imgNew = np.zeros_like(img, np.uint8)
     for button in buttonList:
         x, y = button.position
         width, height = button.size
-        # button - position, size, color
+        cvzone.cornerRect(imgNew, (button.position[0], button.position[1], button.size[0], button.size[1]), 20, rt=0)
+
+        # button
         if "#" in button.text:
-            cv2.rectangle(img, button.position, (x + width, y + height), (0, 0, 0), cv2.FILLED)
+            # black buttons
+            cv2.rectangle(imgNew, button.position, (x + width, y + height), (0, 0, 0), cv2.FILLED)
         else:
-            cv2.rectangle(img, button.position, (x + width, y + height), (255, 255, 255), cv2.FILLED)
+            # white buttons
+            cv2.rectangle(imgNew, button.position, (x + width, y + height), (255, 255, 255), cv2.FILLED)
         # text
-        cv2.putText(img, button.text, (x, y + 65), cv2.FONT_HERSHEY_PLAIN, 1, (255, 0, 255), 1)
-    return img
+        cv2.putText(imgNew, button.text, (x, y + 65), cv2.FONT_HERSHEY_PLAIN, 1, (255, 0, 255), 1)
+    out = img.copy()
+    alpha = 0.5
+    mask = imgNew.astype(bool)
+    print(mask.shape)
+    out[mask] = cv2.addWeighted(img, alpha, imgNew, 1 - alpha, 0)[mask]
+    return out
 
 # TODO: We need 27 blacks and 39 white buttons (out of 36 blacks and 52 whites)
 class Button:
@@ -40,26 +53,53 @@ keys = ["A0", "A#0", "B0",
         "C4", "C#4", "D4", "D#4", "E4", "F4", "F#4", "G4", "G#4", "A4", "A#4", "B4",
         "C5", "C#5", "D5", "D#5", "E5", "F5", "F#5", "G5", "G#5", "A5", "A#5", "B5",
         "C6", "C#6", "D6"]
+
+
 # cool way in python to keep track of the index when iterate using enhanced for loop
 for x, key in enumerate(keys):
     # size is none -> default size is used
     buttons.append(Button([38 * x + 19, 1240], key))
 
+def isPressed(landmarkList, button):
+    x, y = button.position
+    width, height = button.size
+    # flip horizontally by taking the difference from 2560 (which is the width of the display screen)
+    return ((x < 2560 - landmarkList[4][0] < x + width and y < landmarkList[4][1] < y + height) or
+                    (x < 2560 - landmarkList[8][0] < x + width and y < landmarkList[8][1] < y + height) or
+                    (x < 2560 - landmarkList[12][0] < x + width and y < landmarkList[12][1] < y + height) or
+                    (x < 2560 - landmarkList[16][0] < x + width and y < landmarkList[16][1] < y + height) or
+                    (x < 2560 - landmarkList[20][0] < x + width and y < landmarkList[20][1] < y + height))
 
 while True:
     # every iteration a new image is drawn
     success, img = cap.read()
     # detect the hands
     hands, img = detector.findHands(img)
-    if hands:
-        hand = hands[0]
-        landmarkList = hand["lmList"]
-        boundingBox = hand["bbox"]
 
     # Flip the image horizontally for display
     img = cv2.flip(img, 1)
 
-    img = drawAll(img, buttons)
+    img = drawAllTransparent(img, buttons)
+
+    if hands:
+        hand1 = hands[0]
+        landmarkList1 = hand1["lmList"]
+        boundingBox1 = hand1["bbox"]
+        if len(hands) == 2:
+            hand2 = hands[1]
+            landmarkList2 = hand2["lmList"]
+            boundingBox2 = hand2["bbox"]
+
+        # check if finger clicking a button
+        for button in buttons:
+            x, y = button.position
+            width, height = button.size
+            # landmarkList[8] is the index finger, 12 is the middle, etc.
+            # see https://ai.google.dev/edge/mediapipe/solutions/vision/hand_landmarker
+            if (isPressed(landmarkList1, button)) or (len(hands) == 2 and isPressed(landmarkList2, button)):
+                # pressed buttons are colored green
+                cv2.rectangle(img, button.position, (x + width, y + height), (0, 255, 0), cv2.FILLED)
+                cv2.putText(img, button.text, (x, y + 65), cv2.FONT_HERSHEY_PLAIN, 1, (255, 255, 255), 1)
 
     cv2.imshow("Image", img)
     cv2.waitKey(1)
